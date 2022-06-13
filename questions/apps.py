741198b65bf7c93c45830a916086dba207243fe5
Jack M <jackm550@googlemail.com>
2022-06-13 11:03:05 +0100

    added Django signals to automatically generate a slug on question creation

--- questions/apps.py
+++ questions/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class QuestionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'questions'
+
+    #import signals when ready
+    def ready(self):
+        import questions.signals
