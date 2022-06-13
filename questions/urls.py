f7721ba497bdf2f0c7908d09b8c1092778879ba4
Jack M <jackm550@googlemail.com>
2022-06-13 14:21:45 +0100

    Question details page drafted

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import question_list
+from .views import question_list, question_details
 
 urlpatterns = [
     path('', question_list, name='question_list'),
+    path("question/<slug:slug>/", question_details, name="question_details"),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 # Create your views here.
 def question_list(request):
     question_list = Question.objects.all().order_by("-created_at")
     return render(request, "question_list.html", {"question_list": question_list})
+
+def question_details(request, slug):
+    question = get_object_or_404(Question, slug=slug)
+    return render(request, "question_details.html", {"question": question})
